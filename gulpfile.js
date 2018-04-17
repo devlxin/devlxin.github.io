421e4df78cebcc9d8d44ec720d36a8362218b7fd
LIXIN <1050421454@qq.com>
2018-04-17 16:36:56 +0800

    new blog

--- gulpfile.js
+++ gulpfile.js
@@ -1,70 +1,71 @@
-const gulp = require('gulp');
-const cp = require('child_process');
-const bs = require('browser-sync');
-const postcss = require('gulp-postcss');
-const cssImport = require('postcss-import');
-const nest = require('postcss-nested');
-const customProps = require('postcss-custom-properties');
-const calc = require('postcss-calc');
-const nano = require('gulp-cssnano');
+var gulp = require('gulp');
+var rename = require('gulp-rename');
+var svg2png = require('gulp-svg2png');
+var ico = require('gulp-to-ico');
 
+var iconBasename = 'icon';
+var iconDestPath = './assets/images/logo';
+var faviconDestPath = './';
 
-// Build jekyll project
-gulp.task('jekyll', done => {
-  cp.spawn('jekyll', ['build', '--drafts', '--quiet', '--future'], { stdio: 'inherit' }).on('close', done);
+function coverSvg(width, height) {
+  height || (height = width);
+  return gulp.src('./assets/images/logo/logo.svg')
+    .pipe(svg2png({ width: width, height: height }))
+    .pipe(rename({
+      basename: iconBasename,
+      suffix: '-' + width + 'x' + height
+    }))
+    .pipe(gulp.dest(iconDestPath));
+}
+
+gulp.task('icon310', function() {
+  return coverSvg(310);
+});
+gulp.task('icon310x150', function() {
+  return coverSvg(310, 150);
+});
+gulp.task('icon192', function() {
+  return coverSvg(192);
+});
+gulp.task('icon180', function() {
+  return coverSvg(180);
+});
+gulp.task('icon167', function() {
+  return coverSvg(167);
+});
+gulp.task('icon152', function() {
+  return coverSvg(152);
+});
+gulp.task('icon150', function() {
+  return coverSvg(150);
+});
+gulp.task('icon128', function() {
+  return coverSvg(128);
+});
+gulp.task('icon120', function() {
+  return coverSvg(120);
+});
+gulp.task('icon70', function() {
+  return coverSvg(70);
+});
+gulp.task('icon48', function() {
+  return coverSvg(48);
+});
+gulp.task('icon16', function() {
+  return coverSvg(16);
 });
 
-// Rebuild and refresh project
-gulp.task('reload', ['jekyll'], () => {
-  bs.reload();
+gulp.task('favicon', ['icon128', 'icon48', 'icon16'], function() {
+  return gulp.src([
+    iconDestPath + '/icon-128x128.png',
+    iconDestPath + '/icon-48x48.png',
+    iconDestPath + '/icon-16x16.png'])
+      .pipe(ico({ path: 'favicon.ico'}))
+      .pipe(gulp.dest(faviconDestPath));
 });
 
-// Start BrowserSync server and serve _site directory
-gulp.task('browser-sync', ['styles', 'jekyll'], () => {
-  bs({
-    ui: false,
-    ghostMode: {
-      clicks: true,
-      forms: false,
-      scroll: true
-    },
-    logPrefix: 'songroger',
-    notify: false,
-    // port: 4000,
-    server: {
-      baseDir: '_site'
-    }
-  });
-});
+gulp.task('icons', [
+  'icon310', 'icon310x150', 'icon192', 'icon180', 'icon167', 'icon152', 'icon150', 'icon120', 'icon70'
+]);
 
-// Process css, autoprefix, minify
-gulp.task('styles', () => {
-  const processors = [
-    cssImport,
-    customProps,
-    calc,
-    nest
-  ];
-  return gulp.src('_src/css/main.css')
-    .pipe(postcss(processors))
-    .pipe(nano({
-          //the following option is for :CSS3 Animation is missing after minify
-          reduceIdents: {
-              keyframes: false
-          },
-          discardUnused: {
-              keyframes: false
-          }
-          })
-    )
-    .pipe(gulp.dest('_includes/css'));
-});
-
-// Watch sass and all html posts
-gulp.task('watch', () => {
-  gulp.watch('_src/css/**/*.css', ['styles', 'reload']);
-  gulp.watch(['index.html', '_layouts/*.html', '_includes/*.html', '_posts/*', '_drafts/*', '*.md'], ['reload']);
-});
-
-// default task
-gulp.task('default', ['styles', 'watch']);
+gulp.task('artwork', ['favicon', 'icons']);
